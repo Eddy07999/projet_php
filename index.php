938a710d3b113b342ee789f3a40f2a6b1b3c087f
leullier <eddy.leullier@gmail.com>
2022-11-16 16:06:56 +0100

    connexion bdd

--- index.php
+++ index.php
@@ -9,7 +9,50 @@
 </head>
 
 <body>
-    <?php echo ' Coucou' ?>
+    <?php
+
+
+    try {
+        $ipserver = "localhost";
+
+        $nomBase = "projet_php";
+        $loginPrivilege = "userWeb";
+        $passwordPrivilege = "password";
+
+
+        $pdo = new PDO('mysql:host=' . $ipserver . ';dbname=' . $nomBase . '', $loginPrivilege, $passwordPrivilege);
+        ;
+        echo "connexion ok";
+    } catch (Exception $error) {
+        $error->getMessage();
+    }
+
+    if (isset($_POST['connexion'])) {
+        echo "vous avez saisi" . " " . $_POST['login'] . " et " . $_POST['pass'] . " en password ";
+        $requeteSql = "SELECT * FROM `User` WHERE `login`='" . $_POST['login'] . "' AND `pass`='" . $_POST['pass'] . "';";
+
+        $resultat = $pdo->query($requeteSql);
+
+        if ($resultat->rowCount() > 0) {
+            echo ('c est tout bon');
+        } else {
+            echo ('C esst pas bon');
+        }
+
+    } else {
+        echo "veuillez vous identifier";
+    }
+
+
+    ?>
+
+    <form action="" method="post">
+        Login: <input type="text" name="login" />
+        Pass: <input type="password" name="pass" />
+        <input type="submit" name="connexion" />
+
+    </form>
+
 </body>
 
 </html>
